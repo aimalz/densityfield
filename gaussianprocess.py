49e2ad844624d14c8d667b6d650ede99d902a125
“DrMud” <madhura.killedar.astro@gmail.com>
2016-08-31 10:43:07 -0700

    tweak and add plots; fix arguments to function

--- gaussianprocess.py
+++ gaussianprocess.py
@@ -2,29 +2,43 @@ from __future__ import print_function
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import simps
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
 
 filepath = "../ics_matterpow_0.dat"
 
 data = np.loadtxt(filepath)
-k_scale = data[:,0]
+k_arr = data[:,0]
 power = data[:,1]
 logp = np.log10(power)
+logk = np.log10(k_arr)
 
 fig = plt.figure()
-foo = plt.plot(k_scale,logp*k_scale**2)
+foo = plt.plot(logk,logp)
+plt.xlabel(r"$\log k$")
+plt.ylabel(r"$\log P(k)$")
 plt.show()
 
-def func(k,r):
-    return power * k**2 * np.sin(k*r) / (k*r) / (2.*np.pi)
+fig = plt.figure()
+foo = plt.plot(logk,logp*k_arr**2)
+plt.xlabel(r"$\log k$")
+plt.ylabel(r"$k^{2}\log P(k)$")
+plt.show()
+
+def p2cf(k,pk,r):
+    return pk * k**2 * np.sin(k*r) / (k*r) / (2.*np.pi)
 
 r_arr = np.linspace(1.,1000.,200)
 xi = np.zeros(len(r_arr))
 for i in range(len(r_arr)):
     r = r_arr[i]
-    xi[i] = simps(func(k_scale,r),x=k_scale)
+    xi[i] = simps(p2cf(k_arr,power,r),x=k_arr)
 
 fig = plt.figure()
 foo = plt.plot(np.log10(r_arr),r_arr**2*xi)
+plt.xlabel(r"$\log_{10}r$")
+plt.ylabel(r"$r^{2} \xi(r)$")
 plt.show()
 
 
